50b707ff6ca8ba5313d1948bf6a342710bdb9da4
nshabiirwe kizzah <nksiha.1601@gmail.com>
2023-05-13 19:28:56 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:latest
+WORKDIR /app
+COPY requirement.txt requirements.txt
+RUN pip3 install -r requirements.txt
+COPY . .
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ hosp-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sagittarius/Desktop/CLOUD/hospitalmanagement-master/hosp-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
